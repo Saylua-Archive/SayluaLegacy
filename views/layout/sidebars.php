986419bb6a36ee6ed4be4286aeacd49653c61c10
Tiff Zhang <zhangtiff@gmail.com>
2016-06-02 18:30:10 -0700

    Layout changes

--- views/layout/sidebars.php
+++ views/layout/sidebars.php
@@ -1,13 +1,30 @@
-<div class="sidebar left" id="left-bar">
-  <div id="ha-panel">
-    <a href="/ha"><img src="/static/img/ha.png"></a>
+<div class="sidebar-container">
+  <div class="sidebar left" id="left-bar">
+    <div class="sidebar-section" id="ha-panel">
+      <a href="/ha"><img src="/static/img/ha.png"></a>
+    </div>
+    <div class="sidebar-section">
+      <p>You are <a href="/user">User</a></p>
+      <p>
+        20 Common Currency
+      </p>
+      <p>
+        76 Premium Currency
+      </p>
+      <p>
+        50 Event Curency
+      </p>
+    </div>
   </div>
-  <p>You are <a href="/user">User</a></p>
-</div>
-<div class="sidebar right" id="right-bar">
-  Your Party
-  <a href="/pet"><img src="/static/img/SHC.png" class="left"></a>
-  <a href="/pet"><img src="/static/img/SHC.png" style="width: 50%;" class="left"></a>
-  <a href="/pet"><img src="/static/img/SHC.png" style="width: 50%;" class="left"></a>
-  <p>Smoking Hot Chick is hungry! <a href="/pet">Feed them?</a> </p>
+  <div class="sidebar right" id="right-bar">
+    <div class="sidebar-section">
+      <div class="sidebar-header">Your Party</div>
+      <a href="/pet"><img src="/static/img/SHC.png" class="left"></a>
+    </div>
+    <div class="sidebar-section">
+      <a href="/pet"><img src="/static/img/SHC.png" style="width: 50%;" class="left"></a>
+      <a href="/pet"><img src="/static/img/SHC.png" style="width: 50%;" class="left"></a>
+      <p>Smoking Hot Chick is hungry! <a href="/pet">Feed them?</a> </p>
+    </div>
+  </div>
 </div>
