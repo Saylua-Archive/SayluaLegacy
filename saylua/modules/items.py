5ad9f55d8f6b8d72eca34b91d3cc0c157e81c6a0
Tiffany Zhang <tiffanyzhang@Tiffanys-MacBook.local>
2016-08-30 23:22:03 -0700

    Part 1 of the CSS refactor.

--- saylua/modules/items.py
+++ saylua/modules/items.py
@@ -1,7 +1,15 @@
 from saylua import app
 from flask import (render_template, redirect,
                    url_for, flash, session, abort, request)
+import os, random
 
 @app.route('/inventory/')
 def items_inventory():
-    return render_template("items/inventory.html")
+    path = os.path.join(app.static_folder, 'img/items/160/')
+    items = []
+
+    for i in xrange(60):
+        name = random.choice(os.listdir(path))
+        items.append({'name': name, 'img_url': '/static/img/items/160/' + name})
+
+    return render_template('items/inventory.html', items=items)
