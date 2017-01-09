a0a8fe8f795dcb7501055e220a4899a561cfd057
LikeMyBread <mbradley31@outlook.com>
2017-01-08 18:14:16 -0800

    Added more roles to provisioner

--- saylua/provisioner.py
+++ saylua/provisioner.py
@@ -2,6 +2,7 @@ from saylua.models.role import Role
 
 # To run this import setup in the interactive console and run it as such
 # After that, edit a user's role to be admin to create the first admin
+# Then, flush the memcache to make sure the user's role is updated
 #
 # from saylua.provisioner import setup
 # setup()
@@ -14,5 +15,13 @@ def setup():
     for entry in admin_dict:
         setattr(admin_role, entry, True)
     admin_role.put()
+    print("Admin Role Created")
+
+    # Add the 'user' role
+    user_role = Role(id='admin')
+    user_role.can_post_threads = True
+    user_role.can_comment = True
+    user_role.put()
+    print("User Role Created")
 
     print("Database Setup Complete")
