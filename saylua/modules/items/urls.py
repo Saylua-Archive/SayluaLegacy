afd9351594555d997bddc67f7111c23381413b38
Tiffany Zhang <zhangtiff@gmail.com>
2017-01-18 02:12:37 -0800

    Some light refactoring. Moving files around and such.

--- saylua/modules/items/urls.py
+++ saylua/modules/items/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     # Admin URLs
-    url('/admin/items/add/', view_func=views.admin.admin_panel_items_add, name='items_admin_add', methods=['GET', 'POST']),
-    url('/admin/items/edit/', view_func=views.admin.admin_panel_items_edit, name='items_admin_edit'),
+    url('/admin/items/add/', view_func=views.admin.admin_panel_items_add,
+        name='items_admin_add', methods=['GET', 'POST']),
+    url('/admin/items/edit/', view_func=views.admin.admin_panel_items_edit,
+        name='items_admin_edit'),
 
     # Primary URLs
     url('/inventory/<category>/', view_func=views.items.items_inventory, name='items_inventory'),
