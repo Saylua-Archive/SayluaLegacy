7fc4746a7117b7d24a3c55ac7814815e749728e7
Tiff Zhang <zhangtiff@gmail.com>
2017-01-23 21:53:41 -0800

    Getting the ball rolling on SQLAlchemy support.

--- saylua/config/example_secure.py
+++ saylua/config/example_secure.py
@@ -1,5 +1,10 @@
 #Replace dummy values with the real ones, and rename or copy this file to secure.py
 SECRET_KEY = "dummy"
 INSTANCE_NAME = "saylua-1280:saylua"
+
+# CAPTCHA
 RECAPTCHA_PUBLIC_KEY = 'dummy'
 RECAPTCHA_PRIVATE_KEY = 'dummy'
+
+# Database
+SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/sayluadb'
