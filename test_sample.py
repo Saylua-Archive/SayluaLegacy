5c31ec56e4316af1bef563877043999e143c7ee9
LikeMyBread <mbradley31@outlook.com>
2017-01-26 00:19:24 -0800

    More Travis testing

--- test_sample.py
+++ test_sample.py
@@ -1,6 +1,8 @@
 # content of test_sample.py
+#Note: any files that match test_*.py or *_test.py will be checked for tests to run
 def func(x):
     return x + 1
 
 def test_answer():
-    assert func(3) == 5
+    #assert func(3) == 5
+    assert func(4) == 5
