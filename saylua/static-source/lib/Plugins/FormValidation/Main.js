2b7e5c604c1bba8d63f6fdf177224bef0c502291
Tiffany Zhang <zhangtiff@gmail.com>
2017-03-19 02:11:18 -0700

    Made little errors appear below form fields.

--- saylua/static-source/lib/Plugins/FormValidation/Main.js
+++ saylua/static-source/lib/Plugins/FormValidation/Main.js
@@ -9,34 +9,47 @@ export default class FormValidation {
   }
 
   bind(selector) {
-    var forms = document.querySelectorAll(selector);
-    for (var i = 0; i < forms.length; i++) {
+    let forms = document.querySelectorAll(selector);
+    for (let i = 0; i < forms.length; i++) {
       forms[i].addEventListener('submit', this._formSubmitListener); // End event listener
     }
   }
 
   _formSubmitListener(e) {
-    var errCount = 0;
-    var form = e.currentTarget;
-    var fields = form.querySelectorAll('input, textarea');
-    var errorContainer = document.getElementById(form.getAttribute('data-error-id'));
+    let errCount = 0;
+    let form = e.currentTarget;
+    let fields = form.querySelectorAll('input, textarea');
+
+    // DOM element for showing all errors in one place.
+    let errorContainer = document.getElementById(form.getAttribute('data-error-id'));
+    let errorFields = form.getElementsByClassName('form-error');
+    let errorFieldMap = {};
+    for (let i = 0; i < errorFields.length; i++) {
+      let field = errorFields[i];
+      if (field.getAttribute('data-field-name')) {
+        // Oh noes, side effects in my reduce! D:
+        field.innerHTML = '';
+        errorFieldMap[field.getAttribute('data-field-name')] = field;
+      }
+    }
+
     if (errorContainer) {
       errorContainer.innerHTML = '';
     }
-    for (var j = 0; j < fields.length; j++) {
-      var validators = fields[j].getAttribute('data-slform-validators');
+    for (let j = 0; j < fields.length; j++) {
+      let validators = fields[j].getAttribute('data-slform-validators');
       if (validators) {
         removeClass(fields[j], 'error');
         validators = validators.split(" ");
-        var noErrors = true;
-        for (var k = 0; noErrors && k < validators.length; k++) {
-          var params = validators[k].split(":");
-          var validatorName = params[0];
+        let noErrors = true;
+        for (let k = 0; noErrors && k < validators.length; k++) {
+          let params = validators[k].split(":");
+          let validatorName = params[0];
           if(!validatorList[validatorName]) {
             throw "Invalid validator type: " + validatorName;
           }
 
-          var value = fields[j].value;
+          let value = fields[j].value;
           if (fields[j].type == 'checkbox') {
             value = fields[j].checked;
           }
@@ -47,21 +60,28 @@ export default class FormValidation {
           // Check to see if the validation fails
           if (!validatorList[validatorName].validator.apply(this, params)) {
             addClass(fields[j], 'error');
-            var field = capitalizeFirst(fields[j].name);
-            var err;
-            var message = fields[j].getAttribute('data-slform-' + validatorName + '-message');
+            let field = capitalizeFirst(fields[j].name);
+            let err;
+            let message = fields[j].getAttribute('data-slform-' + validatorName + '-message');
             if (message) {
               err = message;
             } else {
               err = validatorList[validatorName].error;
             }
             err = err.replace('<field>', field.replace(/[_-]/g, ' '));
-            for (var l = 1; l < params.length; l++) {
+            for (let l = 1; l < params.length; l++) {
               err = err.replace('<' + l + '>', params[l]);
             }
             errCount++;
             noErrors = false;
-            errorContainer.innerHTML += '<p class="error">' + err + '</p>';
+
+            if (errorContainer) {
+              errorContainer.innerHTML += '<p class="error">' + err + '</p>';
+            }
+
+            if (errorFieldMap[fields[j].name]) {
+              errorFieldMap[fields[j].name].innerHTML = err;
+            }
           }
         }
       }
