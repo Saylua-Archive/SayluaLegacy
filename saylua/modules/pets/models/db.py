ebb5af58cf5c4705e6957d3b63493a4297536771
LikeMyBread <mbradley31@outlook.com>
2017-04-24 07:46:30 -0700

    Made suggested changes

--- saylua/modules/pets/models/db.py
+++ saylua/modules/pets/models/db.py
@@ -1,20 +1,6 @@
 from saylua import db
 
 
-# This is to store alternate linart versions of the same pets
-class SpeciesVersion(db.Model):
-
-    __tablename__ = "species_versions"
-
-    id = db.Column(db.Integer, primary_key=True)
-    species_name = db.Column(db.String, db.ForeignKey("species.name"))
-    name = db.Column(db.String(80), unique=True)
-    base_image = db.Column(db.String(200))
-    base_psd = db.Column(db.String(200))
-    default_image = db.Column(db.String(200))
-    species = db.relationship("Species", back_populates="species_versions")
-
-
 # Pets are divided into species and species are divided into variations
 class Species(db.Model):
 
@@ -22,12 +8,11 @@ class Species(db.Model):
 
     name = db.Column(db.String, primary_key=True)
     description = db.Column(db.Text)
-    versions = db.relationship("SpeciesVersion", back_populates="species")
 
 
-class SpeciesVariation(db.Model):
+class SpeciesCoat(db.Model):
 
-    __tablename__ = "species_variations"
+    __tablename__ = "species_coats"
 
     id = db.Column(db.Integer, primary_key=True)
     species_name = db.Column(db.String, db.ForeignKey("species.name"))
@@ -44,12 +29,13 @@ class Pet(db.Model):
 
     owner_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     # Only set if the pet is a variation
-    variation_key = db.Column(db.Integer, db.ForeignKey("users.id"))
-    species_name = db.Column(db.String(80), unique=True) # Note the denormalization
+    coat_id = db.Column(db.Integer, db.ForeignKey("species_coats.id"))
+    coat = db.relationship("SpeciesCoat")
+    species_name = db.Column(db.String, db.ForeignKey("species.name"))
+    species = db.relationship("Species")
 
     # Personal profile information for the pet
     name = db.Column(db.String(80))
-    css = db.Column(db.Text)
     description = db.Column(db.Text)
 
     # If either of these is set to a number other than 0, the pet is for sale
