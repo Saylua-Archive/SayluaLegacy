8fe8cf7b06765776a3072c8524f9a569682239a9
LikeMyBread <mbradley31@outlook.com>
2017-05-09 23:37:00 -0700

    Adopting, accompanying and abandoning companions.

--- saylua/modules/pets/urls.py
+++ saylua/modules/pets/urls.py
@@ -2,6 +2,6 @@ from saylua.routing import url
 from . import views
 
 urlpatterns = [
-    url('/pet/<name>/', view_func=views.pet_profile, name="pets_profile"),
+    url('/pet/<name>/', view_func=views.pet_profile, name="pets_profile", methods=['GET', 'POST']),
     url('/reserve/', view_func=views.pet_reserve, name="pets_reserve", methods=['GET', 'POST'])
 ]
