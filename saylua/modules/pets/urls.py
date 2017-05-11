b2f9233a8e154471aee19f8734bef16c5e71b109
LikeMyBread <mbradley31@outlook.com>
2017-05-10 22:13:46 -0700

    Cleaning up pets with new routes and removed some redundancy.

--- saylua/modules/pets/urls.py
+++ saylua/modules/pets/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url('/pet/<name>/', view_func=views.pet_profile, name="pets_profile", methods=['GET', 'POST']),
-    url('/reserve/', view_func=views.pet_reserve, name="pets_reserve", methods=['GET', 'POST'])
+    url('/reserve/', view_func=views.pet_reserve, name="pets_reserve", methods=['GET', 'POST']),
+    url('/abandon/', view_func=views.pet_abandon, name="pets_abandon", methods=['POST']),
+    url('/accompany/', view_func=views.pet_accompany, name="pets_accompany", methods=['POST'])
 ]
--- saylua/modules/pets/views.py
+++ saylua/modules/pets/views.py
@@ -4,54 +4,56 @@ from .models.db import Pet
 
 
 def pet_profile(name):
-    if request.method == 'POST':
-        if not g.user:
-            flash("You need to be logged in to do this!", "error")
-            return redirect("/login")
-        companion_id = request.form.get('accompany')
-        abandon_id = request.form.get('abandonee')
-        if companion_id:
-            companion = db.session.query(Pet).filter(Pet.id == companion_id).one_or_none()
-            if companion is None or companion.owner_id != g.user.id:
-                flash("You can't accompany this companion.")
-            else:
-                current_companion = g.user.companion
-                if not current_companion:
-                    g.user.companion_id = companion.id
-                    db.session.add(g.user)
-                    db.session.commit()
-                    flash(companion.name + " is now accompanying you!")
-                elif current_companion.id == companion.id:
-                    g.user.companion_id = None
-                    db.session.add(g.user)
-                    db.session.commit()
-                    flash(companion.name + " has gone back to your den.")
-                else:
-                    g.user.companion_id = companion.id
-                    db.session.add(g.user)
-                    db.session.commit()
-                    flash(companion.name + " is now accompanying you! " +
-                        current_companion.name + " has gone back to your den.")
-        elif abandon_id:
-            abandonee = db.session.query(Pet).filter(Pet.id == abandon_id).one_or_none()
-            if abandonee is None or abandonee.owner_id != g.user.id:
-                flash("You can't abandon this companion.")
-            else:
-                if g.user.companion_id == abandonee.id:
-                    g.user.companion_id = None
-                    db.session.add(g.user)
-                    abandonee.owner_id = None
-                    db.session.add(abandonee)
-                    db.session.commit()
-                else:
-                    abandonee.owner_id = None
-                    db.session.add(abandonee)
-                    db.session.commit()
-
     pet = db.session.query(Pet).filter(Pet.soul_name == name).one_or_none()
     if pet is None:
         return render_template('404.html'), 404
     return render_template("profile.html", pet=pet)
+
+
+def pet_accompany():
+    if not g.user:
+        flash("You need to be logged in to do this!", "error")
+        return redirect("/login")
+    companion_id = request.form.get('accompany')
+    if companion_id:
+        companion = db.session.query(Pet).filter(Pet.id == companion_id).one_or_none()
+        if companion is None or companion.owner_id != g.user.id:
+            flash("You can't accompany this companion.")
+        else:
+            current_companion = g.user.companion
+            if not current_companion:
+                g.user.companion_id = companion.id
+                flash(companion.name + " is now accompanying you!")
+            elif current_companion.id == companion.id:
+                g.user.companion_id = None
+                flash(companion.name + " has gone back to your den.")
+            else:
+                g.user.companion_id = companion.id
+                flash(companion.name + " is now accompanying you! " +
+                    current_companion.name + " has gone back to your den.")
+            db.session.add(g.user)
+            db.session.commit()
+    return redirect('/pet/' + companion.soul_name, code=302)
+
+
+def pet_abandon():
+    if not g.user:
+        flash("You need to be logged in to do this!", "error")
+        return redirect("/login")
+    abandon_id = request.form.get('abandonee')
+    abandonee = db.session.query(Pet).filter(Pet.id == abandon_id).one_or_none()
+    if abandonee is None or abandonee.owner_id != g.user.id:
+        flash("You can't abandon this companion.")
+    else:
+        if g.user.companion_id == abandonee.id:
+            g.user.companion_id = None
+            db.session.add(g.user)
+            abandonee.owner_id = None
+        else:
+            abandonee.owner_id = None
+        db.session.add(abandonee)
+        db.session.commit()
+    return redirect('/pet/' + abandonee.soul_name, code=302)
 
 
 def pet_reserve():
