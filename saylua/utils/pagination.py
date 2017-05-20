a727a59c8b378301a3971ed1a71c4107db2d09de
LikeMyBread <mbradley31@outlook.com>
2017-05-19 23:33:53 -0700

    Figuring out the pagination class.

--- saylua/utils/pagination.py
+++ saylua/utils/pagination.py
@@ -1,5 +1,9 @@
+from flask import Markup
+
+
 class PageData:
     def __init__(self, model, per_page, page_number=1):
         self.model = model
         self.per_page = per_page
         self.page_number = page_number
+        self.pagination = Markup()
