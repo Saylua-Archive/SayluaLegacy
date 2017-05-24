06a8b194b0b6ca463bdc5747906508e488a91015
LikeMyBread <mbradley31@outlook.com>
2017-05-23 23:41:19 -0700

    Quick pet action changes.

--- saylua/modules/pets/urls.py
+++ saylua/modules/pets/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url('/pet/<name>/', view_func=views.pet_profile, name="profile", methods=['GET', 'POST']),
+    url('/edit_pet/<name>/', view_func=views.pet_profile, name="profile", methods=['GET', 'POST']),
     url('/reserve/', view_func=views.pet_reserve, name="reserve", methods=['GET']),
     url('/reserve/', view_func=views.pet_reserve_post, name="reserve_post", methods=['POST']),
     url('/abandon/', view_func=views.pet_abandon, name="abandon", methods=['POST']),
