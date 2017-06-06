b88226b6a26b6d525997f215762022e14eca4ea6
LikeMyBread <mbradley31@outlook.com>
2017-06-05 23:35:01 -0700

    Added uploading item images to google cloud storage, still need to test on staging before merging.

--- saylua/modules/items/admin.py
+++ saylua/modules/items/admin.py
@@ -1,4 +1,4 @@
-from saylua import db
+from saylua import app, db
 
 from saylua.utils import get_from_request
 from saylua.wrappers import admin_access_required
@@ -7,7 +7,7 @@ from .models.db import Item
 
 from flask import render_template, redirect, url_for, flash, request
 from forms import ItemUploadForm
-# import cloudstorage as gcs
+import cloudstorage as gcs
 
 
 @admin_access_required
@@ -20,6 +20,16 @@ def admin_panel_items_add():
         form.populate_obj(item)
         db.session.add(item)
         db.session.commit()
+        item_image = request.form.get('image')
+        filename = app.config['IMAGE_BUCKET_NAME'] + "/items/" + item.canon_name + ".png"
+        write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+        gcs_file = gcs.open(filename,
+                          'w',
+                          content_type='image/png',
+                          options={},
+                          retry_params=write_retry_params)
+        gcs_file.write(item_image.encode('utf-8'))
+        gcs_file.close()
         flash('You have successfully created a new item!')
         return redirect(url_for('items.admin_add'))
     return render_template('admin/add.html', form=form)
