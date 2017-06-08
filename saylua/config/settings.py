8e13576b2f9ab22bd2f7a225e925b05ac8494214
LikeMyBread <mbradley31@outlook.com>
2017-06-07 22:28:49 -0700

    Fixed url for static bucket

--- saylua/config/settings.py
+++ saylua/config/settings.py
@@ -2,7 +2,7 @@ DEBUG = True
 SESSION_LIMIT = 5
 COOKIE_DURATION = 30
 MAIN_URL_ROOT = 'https://saylua.com'
-IMAGE_BUCKET_ROOT = 'https://storage.cloud.google.co/saylua-images'
+IMAGE_BUCKET_ROOT = 'https://storage.cloud.google.com/saylua-images'
 IMAGE_BUCKET_NAME = '/saylua-images'
 
 # Form fields
