2aa14a484201f7d20e400da59b0a06a012187328
LikeMyBread <mbradley31@outlook.com>
2017-07-12 23:55:35 -0700

    Added religious figures and infamous historical figures to removed_words list, formerly known as bad_words.

--- saylua/modules/pets/soul_names.py
+++ saylua/modules/pets/soul_names.py
@@ -1,6 +1,6 @@
 import random
 from namelist import name_list
-from badwords import bad_words
+from removedwords import removed_words
 
 
 def soul_name(min_length=7):
@@ -10,7 +10,7 @@ def soul_name(min_length=7):
     start = start[0:random.randint(2, len(start) - 1)]
     while len(start) < min_length:
         start = extend_name(start)
-    if is_bad(start):
+    if is_removed(start):
         return soul_name(min_length) # try again if fails the check
     return start.lower()
 
@@ -26,9 +26,9 @@ def extend_name(name):
     return returnname
 
 
-def is_bad(word):
+def is_removed(word):
     word = word.lower()
-    for testword in bad_words:
+    for testword in removed_words:
         if testword in word:
             return True
     return False
