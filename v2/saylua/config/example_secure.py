d3adec9cd78a90c2b02ab68bcb7019eddc95c9c2
Tiffany Zhang <zhangtiff@gmail.com>
2017-07-13 23:29:26 -0700

    Really basic v2.

---
+++ v2/saylua/config/example_secure.py
@@ -0,0 +1,14 @@
+# Replace dummy values with the real ones, and rename or copy this file to secure.py
+SECRET_KEY = "dummy"
+INSTANCE_NAME = "saylua-1280:saylua"
+
+# CAPTCHA
+RECAPTCHA_PUBLIC_KEY = 'dummy'
+RECAPTCHA_PRIVATE_KEY = 'dummy'
+
+# Database
+SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/sayluadb'
+
+# API KEYS
+MAILGUN_API_KEY = 'dummy'
+MAILGUN_VALIDATION_KEY = 'validate_emails'
