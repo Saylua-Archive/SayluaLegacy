f34f56e358c62e3b48c190ace8f46aa2e6aa2bb5
LikeMyBread <mbradley31@outlook.com>
2017-07-15 22:47:38 -0700

    finished npc style cleanup

--- saylua/modules/world/views.py
+++ saylua/modules/world/views.py
@@ -1,8 +1,20 @@
-from flask import render_template
+from flask import render_template, g, redirect, flash
+from saylua.wrappers import login_required
+from saylua import db
+from saylua.modules.items.models.db import Item
+from operator import attrgetter
 
 
 def town_main():
     return render_template("town.html")
 
+
+@login_required(redirect='views.free_items', error='You need to be logged to get free items!')
 def free_items():
-    return render_template("free_items.html")
+    candidates = Item.query.order_by(db.func.random()).limit(3).all()
+    recipient = g.user
+    for candidate in candidates:
+        flash(candidate.name + " " + str(candidate.buyback_price))
+    item = min(candidates, key=attrgetter('buyback_price'))
+    flash(item.name)
+    return redirect('/home/', code=302)
