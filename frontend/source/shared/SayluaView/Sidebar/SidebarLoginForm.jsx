6e6f918cb1e5ec321b3e8f63b7e8cc22f184d6b8
Tiffany Zhang <zhangtiff@google.com>
2017-09-15 21:51:39 -0700

    Reenable form CSS.

--- frontend/source/shared/SayluaView/Sidebar/SidebarLoginForm.jsx
+++ frontend/source/shared/SayluaView/Sidebar/SidebarLoginForm.jsx
@@ -9,32 +9,34 @@ let SidebarLoginForm = (props) =>  {
     <form id="sidebar-login-form" onSubmit={ handleSubmit }
         className="sidebar-login-form" action="/login/" method="post">
       <h3>Login to Saylua</h3>
-      <table className="form-table center">
-        <tr>
-          <td>
-            <label htmlFor="username">Username</label>
-            <Field name="username" component="input" type="text" />
-            <small className="form-tip"></small>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <label htmlFor="password">Password</label>
-            <Field name="password" component="input" type="password" />
-            <small className="form-tip"></small>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <button type="submit" className="small" name="login">Login!</button>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <p><Link to="/login/recover/">Lost credentials?</Link></p>
-            <p><Link to="/register/">Register!</Link></p>
-          </td>
-        </tr>
+      <table className="form-table">
+        <tbody>
+          <tr>
+            <td>
+              <label htmlFor="username">Username</label>
+              <Field name="username" component="input" type="text" />
+              <small className="form-tip"></small>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="password">Password</label>
+              <Field name="password" component="input" type="password" />
+              <small className="form-tip"></small>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button type="submit" className="small" name="login">Login!</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p><Link to="/login/recover/">Lost credentials?</Link></p>
+              <p><Link to="/register/">Register!</Link></p>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </form>
   );
